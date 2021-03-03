4ec01e9f712e4053ea587aa44ce5164ae0845372
washopilot <washopilot@yahoo.com>
2021-03-02 22:31:50 -0500

    animación de overlay -visibility:hidden!!!

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('header');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function () {
   console.log('click hamburger');
 
   if (header.classList.contains('open')) {
+    // Close Hamburger Menu
     header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   } else {
+    // open Hambuerger Menu
     header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   }
 });
