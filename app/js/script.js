8cae248cb71c60d58de0b460e67062ece43da5c2
washopilot <washopilot@yahoo.com>
2021-03-02 21:42:53 -0500

    arreglo de clase 'open' para HamburgerMenu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('header');
+
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger');
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+  } else {
+    header.classList.add('open');
+  }
+});
